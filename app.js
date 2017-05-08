e351038d99e003301fb8cd253ca0987a20675e84
Beatriz Silva Villa Alvarez <bia@tecgraf.puc-rio.br>
2017-05-08 08:47:42 -0300

    Exercicio semana 7

---
+++ app.js
@@ -0,0 +1,21 @@
+(function () {
+'use strict';
+
+angular.module('MyLunchChecker', [])
+//.controller('myLunchCheckerController', myLunchCheckerController);
+.controller('MyLunchCheckerController', function ($scope, $filter, $injection) {
+
+  $scope.list = [];
+  $scope.message="Lets check how much foodf you are eating!"
+  $scope.checkLunch = function () {
+// fazer o parser e incrementar
+  };
+});
+
+  $scope.displayMessage = function () {
+    var message =  "Lets check how much foodf you are eating!"
+    //$scope.message = "Lets check how much foodf you are eating!"
+    $scope.
+  };
+
+})();

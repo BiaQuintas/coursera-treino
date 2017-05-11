88b240604c31a0fbe1565ebfb2bdfb10128003d0
Beatriz Silva Villa Alvarez <bia@tecgraf.puc-rio.br>
2017-05-11 10:16:22 -0300

    Trying again

--- angularjs/semana1/app.js
+++ angularjs/semana1/app.js
@@ -5,36 +5,35 @@ angular.module('LunchChecker', [])
 .controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
-
-
 fucntion LunchCheckController ($scope){
   $scope.dishList = "";
   $scope.message = "";
   $scope.color = "black";
 
-  $scope.checkLunch = function () {
+  $scope.checkLunch = function() {
     //if (!$scope.dishList)
-    if ( $scope.dishList == undefined || $scope.dishList==""){
-        $scope.message = "Please enter data first";
-        $scope.color = "red";
-        return;
-    }
-        var listDishes = $scope.dishList.split(",");
+      var listDishes = $scope.dishList.split(",");
+      var count = 0;
+    //if ( listDishes == undefined || listDishes==""){
+      if (!listDishes){
+          $scope.message = "Please enter data first";
+          $scope.color = "red";
+          return;
+        }
 
-    var count = 0;
-    for (count = 0; count<listDishes.length; count++){
-      if(listDishes[count].trim() != ""){
-        count++;
+      for (count = 0; count<listDishes.length; count++){
+        if(listDishes[count].trim() != ""){
+          count++;
+        }
       }
-    }
 
-    if (count<=3){
-      $scope.message = "Enjoy!";
-      $scope.color = "green";
-    } else {
-      $scope.message = "Too much!";
-      $scope.color = "red";
-    }
-  };
-}
+      if (count<=3){
+        $scope.message = "Enjoy!";
+        $scope.color = "green";
+      } else {
+        $scope.message = "Too much!";
+        $scope.color = "red";
+      }
+    };
+  }
 })();
